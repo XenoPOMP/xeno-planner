66083ffc02b9255a09c99274929875d21dad65cc
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-18 22:05:37 +0300

    📕 create story for DashboardMenuItem

---
+++ src/components/layout/DashboardMenuItem/DashboardMenuItem.stories.ts
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { KanbanSquareIcon } from 'lucide-react';
+
+import DashboardMenuItem from './DashboardMenuItem';
+
+const meta = {
+  title: 'Dashboard layout/Menu item',
+  component: DashboardMenuItem,
+  tags: ['autodoc'],
+  parameters: {
+    // layout: 'centered',
+  },
+} satisfies Meta<typeof DashboardMenuItem>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    href: '/',
+    'aria-disabled': false,
+    icon: KanbanSquareIcon,
+    children: 'Menu item',
+  },
+};
