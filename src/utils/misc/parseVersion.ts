2e4883783d5a8f4b3393e763aca97faab54a0711
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-18 22:35:20 +0300

    ✍️ update comment

--- src/utils/misc/parseVersion.ts
+++ src/utils/misc/parseVersion.ts
@@ -14,6 +14,11 @@ export interface VersionData {
  * Parses version with preid.
  *
  * @param raw
+ *
+ * @example
+ * const { version, preid } = parseVersion('0.0.0-beta.0');
+ * // version = '0.0.0'
+ * // preid = 'beta'
  */
 export const parseVersion = (raw: string): VersionData => {
   const allPreids: Array<Preid> = ['alpha', 'beta', 'rc'];
