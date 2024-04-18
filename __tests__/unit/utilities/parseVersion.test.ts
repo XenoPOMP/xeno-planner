0454aff34a532edaaf0d090d5e7fb2473f12aea9
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-18 22:31:56 +0300

    🗝️ create parseVersion func

---
+++ __tests__/unit/utilities/parseVersion.test.ts
@@ -0,0 +1,35 @@
+import { describe, test } from 'vitest';
+
+import { expectToDeepEqual } from '@/__tests__/assets/utilities';
+import { type VersionData, parseVersion } from '@/src/utils/misc';
+
+const testParseVersion = (raw: string, expected: VersionData) => {
+  expectToDeepEqual(parseVersion(raw), expected);
+};
+
+describe('parseVersion func', () => {
+  test('Version can be null', () => {
+    testParseVersion('Random string', {
+      version: null,
+    });
+  });
+
+  test('Preid can be undefined', () => {
+    testParseVersion('0.0.0', {
+      version: '0.0.0',
+    });
+  });
+
+  test('Preid is parsed correctly', () => {
+    testParseVersion('0.0.0-beta.0', {
+      version: '0.0.0',
+      preid: 'beta',
+    });
+  });
+
+  test('Not allowed preids will be skipped', () => {
+    testParseVersion('0.0.0-notPreid.0', {
+      version: '0.0.0',
+    });
+  });
+});
---
+++ src/utils/misc/parseVersion.ts
@@ -0,0 +1,43 @@
+import type { Nullable } from '@xenopomp/advanced-types';
+
+export type Preid = 'alpha' | 'beta' | 'rc';
+
+export interface VersionData {
+  /** Version in format: (__``x.x.x``__-beta.x) */
+  version: Nullable<string>;
+
+  /** Version preid. (x.x.x-__``beta``__.x) */
+  preid?: Preid;
+}
+
+/**
+ * Parses version with preid.
+ *
+ * @param raw
+ */
+export const parseVersion = (raw: string): VersionData => {
+  const allPreids: Array<Preid> = ['alpha', 'beta', 'rc'];
+
+  /** Parsed version. */
+  const version = raw.match(/(\d\.){2}\d/gi)?.at(0) || null;
+  const preid = raw.match(/(?<=-)\w+(?=\.\d)/gi)?.at(0);
+
+  /** Preid not found. */
+  if (preid === undefined) {
+    return {
+      version,
+    };
+  }
+
+  /** Not allowed preid. */
+  if (!(allPreids as string[]).includes(preid)) {
+    return {
+      version,
+    };
+  }
+
+  return {
+    version,
+    preid: preid as Preid,
+  };
+};
