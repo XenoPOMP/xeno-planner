428f1c19902f1c12ec3e8b75b1c49f959ddefb4e
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-18 21:55:14 +0300

    👁️ add new pages to sitemap

--- __tests__/unit/app/sitemap.test.tsx
+++ __tests__/unit/app/sitemap.test.tsx
@@ -2,7 +2,8 @@ import { afterAll, beforeAll, describe, test, vi } from 'vitest';
 
 import { clearMocks } from '@/__tests__/assets/mocks';
 import { mockEnv } from '@/__tests__/assets/mocks/mockEnv';
-import { testObject } from '@/__tests__/assets/utilities';
+import { expectToDeepEqual, testObject } from '@/__tests__/assets/utilities';
+import { AppConstants } from '@/app/app.constants.ts';
 import sitemap from '@/app/sitemap';
 
 describe('Next.js sitemap generation', () => {
@@ -21,5 +22,21 @@ describe('Next.js sitemap generation', () => {
   test('Canonical url equals to constant value by default', async () => {
     vi.unstubAllEnvs();
     testObject(sitemap());
+  });
+
+  test('All routes are added to sitemap', () => {
+    const CANONICAL = AppConstants.defaultCanonical;
+
+    const expected: string[] = [
+      CANONICAL,
+      `${CANONICAL}/dashboard`,
+      `${CANONICAL}/tasks`,
+      `${CANONICAL}/pomodoro`,
+      `${CANONICAL}/time-blocking`,
+      `${CANONICAL}/settings`,
+    ];
+    const real = sitemap().map(({ url }) => url);
+
+    expectToDeepEqual(expected, real);
   });
 });
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,4 +1,7 @@
+import type { ArrayType } from '@xenopomp/advanced-types';
+
 import { AppConstants } from '@/app/app.constants';
+import { dashboardMenuData } from '@/src/components/layout/DashboardMenu/dashboard.menu.data.ts';
 import { useEnv } from '@/src/hooks/use-env';
 import { type Sitemap } from '@/src/utils/seo/sitemap-utils';
 
@@ -6,12 +9,21 @@ export default function sitemap(): Sitemap {
   const env = useEnv();
   const CANONICAL = env.get('CANONICAL_URL') || AppConstants.defaultCanonical;
 
+  const dashboardEntries: Sitemap = dashboardMenuData.map<ArrayType<Sitemap>>(
+    ({ href }) => {
+      return {
+        url: `${CANONICAL}${href}`,
+        priority: 1,
+      };
+    },
+  );
   return [
     {
       url: `${CANONICAL}`,
       // lastModified: new Date(2024, Months.February, 25),
-      changeFrequency: 'always',
-      priority: 1,
+      // changeFrequency: 'always',
+      priority: 0.9,
     },
+    ...dashboardEntries,
   ];
 }
