ba071707366edd7e44f8e39643ecb1c9c7e51439
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-20 16:28:40 +0300

    📃 create login and register pages

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,28 +1,28 @@
-import { type Metadata } from 'next';
+import type { PropsWith } from '@xenopomp/advanced-types';
+import cn from 'classnames';
+import type { Metadata } from 'next';
 import { type FC } from 'react';
 
-import Auth from '@/src/components/layout/Auth';
 import { NO_INDEX_PAGE } from '@/src/constants/seo.constants.ts';
 import { generateOpenGraph } from '@/src/utils/seo';
 
 export async function generateMetadata(): Promise<Metadata> {
-  const title = 'Авторизация';
   const description =
     'Войдите в систему, чтобу получить доступ к полному функционалу.';
 
   return {
-    title,
+    // title,
     description,
     openGraph: generateOpenGraph({
-      title,
+      // title,
       description,
     }),
     ...NO_INDEX_PAGE,
   };
 }
 
-const AuthPage: FC<{}> = () => {
-  return <Auth />;
+const AuthLayout: FC<PropsWith<'children', {}>> = ({ children }) => {
+  return <main className={cn('h-dvh flex-center')}>{children}</main>;
 };
 
-export default AuthPage;
+export default AuthLayout;
