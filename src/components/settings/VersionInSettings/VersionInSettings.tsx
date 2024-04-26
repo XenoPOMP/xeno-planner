2d3909a78253d074d811bf17f440c2f785c63eb8
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-26 10:39:13 +0300

    ✍️ update version display

--- src/components/settings/VersionInSettings/VersionInSettings.tsx
+++ src/components/settings/VersionInSettings/VersionInSettings.tsx
@@ -7,19 +7,40 @@ import { parseVersion } from '@/src/utils/misc';
 
 import type { VersionInSettingsProps } from './VersionInSettings.props';
 
+const PreidText: FC<
+  Pick<ReturnType<typeof parseVersion>, 'preid' | 'prerelease'>
+> = ({ preid, prerelease }) => {
+  if (!preid) {
+    return <></>;
+  }
+
+  if (!prerelease) {
+    return <>({preid})</>;
+  }
+
+  return (
+    <>
+      ({preid}.{prerelease})
+    </>
+  );
+};
+
 /**
  * This component displays current app version in
  * settings page.
  * @constructor
  */
 const VersionInSettings: FC<VersionInSettingsProps> = () => {
-  const { version, preid } = parseVersion(packageJson.version);
+  const { version, preid, prerelease } = parseVersion(packageJson.version);
 
   return (
     <SettingGroup>
       <div className={cn('col-span-full p14')}>
-        <span className={cn('italic')}>Версия приложения:</span> {version} (
-        {preid})
+        <span className={cn('italic')}>Версия приложения:</span> {version}{' '}
+        <PreidText
+          preid={preid}
+          prerelease={prerelease}
+        />
       </div>
     </SettingGroup>
   );
