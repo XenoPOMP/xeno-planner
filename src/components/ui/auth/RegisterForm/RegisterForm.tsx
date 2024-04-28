c163479b5c2024c1785ba14acfb4b80a7de110de
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-28 19:34:11 +0300

    ✍️ refactor register page

--- src/components/ui/auth/RegisterForm/RegisterForm.tsx
+++ src/components/ui/auth/RegisterForm/RegisterForm.tsx
@@ -8,21 +8,21 @@ import InputField from '@/src/components/ui/InputField';
 import { FormActions, FormLink } from '@/src/components/ui/auth/Actions';
 import AuthForm from '@/src/components/ui/auth/AuthForm';
 import FieldList from '@/src/components/ui/auth/FieldList';
-import { NO_AUTOCOMPLETE } from '@/src/constants/fields.constants.ts';
 
 import type { RegisterFormProps } from './RegisterForm.props';
 
 const RegisterForm: FC<RegisterFormProps> = () => {
   return (
-    <AuthForm heading={'Регистрация'}>
+    <AuthForm
+      heading={'Регистрация'}
+      noValidate
+    >
       <FieldList>
         <InputField
           icon={Mail}
           description={'Поле ввода электронной почты'}
           placeholder={'Email'}
           type={'email'}
-          formNoValidate
-          {...NO_AUTOCOMPLETE}
         />
 
         <InputField
@@ -30,8 +30,6 @@ const RegisterForm: FC<RegisterFormProps> = () => {
           description={'Поле ввода пароля'}
           placeholder={'Пароль'}
           type={'password'}
-          formNoValidate
-          {...NO_AUTOCOMPLETE}
         />
 
         <InputField
@@ -39,11 +37,9 @@ const RegisterForm: FC<RegisterFormProps> = () => {
           description={'Поле ввода повторного пароля'}
           placeholder={'Повторите пароль'}
           type={'password'}
-          formNoValidate
-          {...NO_AUTOCOMPLETE}
         />
 
-        <Button>Регистрация</Button>
+        <Button type={'submit'}>Регистрация</Button>
       </FieldList>
 
       <FormActions>
