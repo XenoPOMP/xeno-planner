1479fd3f2f0acc7bd94795057d79575598edcef9
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-28 12:58:11 +0300

    🛹 create task service

---
+++ src/services/task.service.ts
@@ -0,0 +1,22 @@
+import { axiosWithAuth } from '@/src/api/interceptors.ts';
+import type { ITaskResponse, TaskFormStateType } from '@/src/types';
+
+export class TaskService {
+  private static BASE_URL = '/user/task';
+
+  static async getTasks() {
+    return axiosWithAuth.get<ITaskResponse[]>(this.BASE_URL);
+  }
+
+  static async createTask(data: TaskFormStateType) {
+    return axiosWithAuth.post(this.BASE_URL, data);
+  }
+
+  static async updateTask(id: string, data: TaskFormStateType) {
+    return axiosWithAuth.put(`${this.BASE_URL}/${id}`, data);
+  }
+
+  static async deleteTask(id: string) {
+    return axiosWithAuth.delete(`${this.BASE_URL}/${id}`);
+  }
+}
