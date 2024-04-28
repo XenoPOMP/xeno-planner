47424261252f205ebe14f217c10dd1f8e5a53b28
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-28 11:44:56 +0300

    📤 export all types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,8 @@
 export * from './InferBoundStoreType';
 export * from './providerType.ts';
+
+// Backend
+export * from './backend/auth.types.ts';
+export * from './backend/task.types.ts';
+export * from './backend/time-block.types.ts';
+export * from './backend/pomodoro.types.ts';
