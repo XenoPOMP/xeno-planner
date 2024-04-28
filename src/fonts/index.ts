4f8e326309ac350ab563a38368e5a754438d6357
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-28 11:19:00 +0300

    ✍️ extend font settings

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -1,3 +1,7 @@
 import { Geologica } from 'next/font/google';
 
-export const MainFont = Geologica({ subsets: ['latin', 'cyrillic'] });
+export const MainFont = Geologica({
+  subsets: ['latin', 'cyrillic'],
+  display: 'swap',
+  variable: '--font-main',
+});
