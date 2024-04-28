a621bd5529bf07ca2cd876637d7d903d20d59c94
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-04-28 17:30:54 +0300

    🌀 add new type for registration

--- src/hooks/useAuthForm.ts
+++ src/hooks/useAuthForm.ts
@@ -1,18 +1,32 @@
-import { type FieldValues, useForm } from 'react-hook-form';
+import { type FieldValues, type SubmitHandler, useForm } from 'react-hook-form';
 
 import { type AuthService } from '@/src/services/auth.service.ts';
+import type { IAuthForm, IRegisterForm } from '@/src/types';
 
 type FormHookProps<TForm extends FieldValues> = Parameters<
   typeof useForm<TForm>
 >;
 
-export const useAuthForm = <TForm extends FieldValues = {}>(
+export const useAuthForm = <
+  TForm extends IAuthForm | IRegisterForm = IAuthForm,
+>(
   type: Parameters<typeof AuthService.main>[0],
   ...[options = { mode: 'onChange' }, ...rest]: FormHookProps<TForm>
 ) => {
   const hookForm = useForm<TForm>(options, ...rest);
 
+  const authSubmitAction: SubmitHandler<TForm> = async () => {};
+
+  /**
+   * Automatically generates submit handler for
+   * you.
+   */
+  const getSubmitHandler = () => {
+    return hookForm.handleSubmit(authSubmitAction);
+  };
+
   return {
     ...hookForm,
+    getSubmitHandler,
   };
 };
--- src/types/backend/auth.types.ts
+++ src/types/backend/auth.types.ts
@@ -3,6 +3,16 @@ import type { User } from '@xeno-planner/backend-types';
 /** Auth form dto type. */
 export interface IAuthForm extends Pick<User, 'email' | 'password'> {}
 
+/**
+ * This interface only exists to handle
+ * register form by hooks.
+ *
+ * Query will still use body object of type {@link IAuthForm}.
+ */
+export interface IRegisterForm extends IAuthForm, Pick<User, 'name'> {
+  repeatPassword: User['password'];
+}
+
 /** User type, but with excluded password. */
 export type SanitizedUser = Omit<User, 'password'>;
 
