a1345ddfc22f30dab1e895165421a5b5e4239892
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-05-04 15:16:54 +0300

    📝 add mocks to grip component

--- src/components/ui/TGrip/TGrip.tsx
+++ src/components/ui/TGrip/TGrip.tsx
@@ -1,6 +1,8 @@
 import { Priority } from '@xeno-planner/backend-types';
+import cn from 'classnames';
 import { type FC } from 'react';
 
+import Checkbox from '@/src/components/ui/Checkbox';
 import PriorityBadge from '@/src/components/ui/PriorityBadge';
 import { columnType } from '@/src/components/ui/TaskTable/TaskTable.tsx';
 
@@ -9,11 +11,21 @@ import type { TGripProps } from './TGrip.props';
 const TGrip: FC<TGripProps> = () => {
   return (
     <>
-      <td {...columnType('grip')}>1</td>
-
-      <td {...columnType('grip')}>2</td>
-
       <td {...columnType('grip')}>
+        <Checkbox>Доделать дизайн</Checkbox>
+      </td>
+
+      <td
+        className={cn('select-none')}
+        {...columnType('grip')}
+      >
+        3 мая, 2024
+      </td>
+
+      <td
+        className={cn('select-none')}
+        {...columnType('grip')}
+      >
         <PriorityBadge priority={Priority.high}>Высокий</PriorityBadge>
       </td>
     </>
