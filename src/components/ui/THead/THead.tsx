469b7bc2bdf545ae18f8728a9fd23c71d7d7e105
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-05-06 12:54:32 +0300

    🪲 match grip layout

--- src/components/ui/THead/THead.tsx
+++ src/components/ui/THead/THead.tsx
@@ -1,15 +1,34 @@
+import cn from 'classnames';
 import { type FC } from 'react';
 
 import { columnType } from '@/src/components/ui/TaskTable/TaskTable.tsx';
 
+import styles from './THead.module.scss';
 import type { THeadProps } from './THead.props';
 
 const THead: FC<THeadProps> = () => {
   return (
     <tr>
-      <th {...columnType('grip')}>Название задачи</th>
-      <th {...columnType('grip')}>Дата</th>
-      <th {...columnType('grip')}>Приоритет</th>
+      <th
+        {...columnType('grip')}
+        className={cn(styles.headCell)}
+      >
+        Название задачи
+      </th>
+
+      <th
+        {...columnType('grip')}
+        className={cn(styles.headCell)}
+      >
+        Дата
+      </th>
+
+      <th
+        {...columnType('grip')}
+        className={cn(styles.headCell)}
+      >
+        Приоритет
+      </th>
     </tr>
   );
 };
