d57274aabae7e4451f0134f132fc771cd665a7a4
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-05-07 20:43:21 +0300

    🪝 extend hook functionality

--- app/(dashboard)/time-blocking/hooks/useTimeBlocks.ts
+++ app/(dashboard)/time-blocking/hooks/useTimeBlocks.ts
@@ -1,29 +1,36 @@
 import { useQuery } from '@tanstack/react-query';
 import { roundNumber } from '@xenopomp/advanced-utils';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import { MINUTES_IN_DAY } from '@/src/constants/time.constants';
 import { TimeBlockService } from '@/src/services/time-block.service.ts';
+import type { ITimeBlockResponse } from '@/src/types';
 
 export const useTimeBlocks = () => {
-  const query = useQuery({
+  const { data, ...query } = useQuery({
     queryKey: ['time blocks', 'list'],
     queryFn: () => TimeBlockService.getTimeBlocks(),
   });
 
+  const [items, setItems] = useState<ITimeBlockResponse[] | undefined>();
+
+  useEffect(() => {
+    setItems(data);
+  }, [data]);
+
   /** Calculated time available for sleep. */
   const hoursLeft = useMemo(() => {
-    if (!query.data) {
+    if (!data) {
       return 0;
     }
 
-    const minutesBlocked = query.data!.reduce(
+    const minutesBlocked = data!.reduce(
       (reducer, next) => reducer + next.duration,
       0,
     );
 
     return roundNumber((MINUTES_IN_DAY - minutesBlocked) / 60, 1);
-  }, [query.data]);
+  }, [data]);
 
-  return { ...query, hoursLeft };
+  return { ...query, data: items, setItems, hoursLeft };
 };
