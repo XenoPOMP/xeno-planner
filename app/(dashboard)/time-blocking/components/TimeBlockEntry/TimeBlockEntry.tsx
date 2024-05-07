3d1efbe1dd378278c96a3765d1dd48f5809fb4db
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-05-07 19:43:59 +0300

    🪝 create deletion functionality

--- app/(dashboard)/time-blocking/components/TimeBlockEntry/TimeBlockEntry.tsx
+++ app/(dashboard)/time-blocking/components/TimeBlockEntry/TimeBlockEntry.tsx
@@ -2,12 +2,16 @@ import cn from 'classnames';
 import { GripVertical, SquarePen, Trash } from 'lucide-react';
 import { type FC } from 'react';
 
+import { useDeleteTimeBlock } from '@/app/(dashboard)/time-blocking/hooks/useDeleteTimeBlock.ts';
+
 import styles from './TimeBlockEntry.module.scss';
 import type { TimeBlockEntryProps } from './TimeBlockEntry.props';
 
 const TimeBlockEntry: FC<TimeBlockEntryProps> = ({
-  block: { color, duration, name },
+  block: { color, duration, name, id },
 }) => {
+  const deleteBlock = useDeleteTimeBlock(id);
+
   return (
     <div
       className={cn(styles.entry)}
@@ -27,7 +31,11 @@ const TimeBlockEntry: FC<TimeBlockEntryProps> = ({
 
       <section className={cn(styles.controls)}>
         <SquarePen className={cn(styles.icon)} />
-        <Trash className={cn(styles.icon)} />
+
+        <Trash
+          className={cn(styles.icon)}
+          onClick={() => deleteBlock(id)}
+        />
       </section>
     </div>
   );
--- app/(dashboard)/time-blocking/hooks/useDeleteTimeBlock.ts
+++ app/(dashboard)/time-blocking/hooks/useDeleteTimeBlock.ts
@@ -1,15 +1,13 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { TimeBlockService } from '@/src/services/time-block.service.ts';
-import type { TimeBlockFormStateType } from '@/src/types';
 
-export const useCreateTimeBlock = () => {
+export const useDeleteTimeBlock = (key?: string) => {
   const queryClient = useQueryClient();
 
   const { mutate } = useMutation({
-    mutationKey: ['time blocks', 'create'],
-    mutationFn: (data: TimeBlockFormStateType) =>
-      TimeBlockService.createTimeBlock(data),
+    mutationKey: ['time blocks', 'delete', key],
+    mutationFn: (id: string) => TimeBlockService.deleteTimeBlock(id),
     onSuccess() {
       queryClient.invalidateQueries({
         queryKey: ['time blocks', 'list'],
