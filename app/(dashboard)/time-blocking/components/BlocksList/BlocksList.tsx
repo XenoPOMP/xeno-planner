31e7a890c19cfbeb01ace820df2420b219286020
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-05-07 20:47:44 +0300

    ⚠️ display warning messages

--- app/(dashboard)/time-blocking/components/BlocksList/BlocksList.tsx
+++ app/(dashboard)/time-blocking/components/BlocksList/BlocksList.tsx
@@ -1,7 +1,14 @@
+import { DndContext, closestCenter } from '@dnd-kit/core';
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
 import type { VariableFC } from '@xenopomp/advanced-types';
 import cn from 'classnames';
 
+import { useTimeBlockDnd } from '@/app/(dashboard)/time-blocking/hooks/useTimeBlockDnd.ts';
 import { useTimeBlocks } from '@/app/(dashboard)/time-blocking/hooks/useTimeBlocks.ts';
+import WarningMessage from '@/src/components/ui/WarningMessage';
 
 import styles from './BlocksList.module.scss';
 import type { BlocksListProps } from './BlocksList.props';
@@ -11,19 +18,38 @@ const BlocksList: VariableFC<'article', BlocksListProps> = ({
   children,
   ...props
 }) => {
-  const { hoursLeft } = useTimeBlocks();
+  const { hoursLeft, data, setItems } = useTimeBlocks();
+  const { handleDragEnd, sensors } = useTimeBlockDnd(data, setItems);
 
   return (
     <article
       className={cn(styles.list, className)}
       {...props}
     >
-      {children && (
-        <section className={cn(styles.droppable)}>{children}</section>
-      )}
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+      >
+        <section className={cn(styles.droppable)}>
+          <SortableContext
+            items={data || []}
+            strategy={verticalListSortingStrategy}
+          >
+            {/* eslint-disable-next-line no-unneeded-ternary */}
+            {children ? children : <div>Нет блоков.</div>}
+          </SortableContext>
+        </section>
+      </DndContext>
 
       <footer className={cn(styles.hoursCounter)}>
-        Остается <strong>{hoursLeft}ч.</strong> для сна
+        {hoursLeft > 0 ? (
+          <>
+            Остается <strong>{hoursLeft}ч.</strong> для сна
+          </>
+        ) : (
+          <WarningMessage>На сон не осталось часов</WarningMessage>
+        )}
       </footer>
     </article>
   );
