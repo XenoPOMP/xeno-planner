e1ee5ae6ec3eab3a37e3058edd842410fe8d98f8
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-05-07 18:53:57 +0300

    🧒 place component children to section

--- app/(dashboard)/time-blocking/components/BlocksList/BlocksList.tsx
+++ app/(dashboard)/time-blocking/components/BlocksList/BlocksList.tsx
@@ -1,12 +1,21 @@
+import type { VariableFC } from '@xenopomp/advanced-types';
 import cn from 'classnames';
-import { type FC } from 'react';
 
 import styles from './BlocksList.module.scss';
 import type { BlocksListProps } from './BlocksList.props';
 
-const BlocksList: FC<BlocksListProps> = () => {
+const BlocksList: VariableFC<'article', BlocksListProps> = ({
+  className,
+  children,
+  ...props
+}) => {
   return (
-    <article className={cn(styles.list)}>
+    <article
+      className={cn(styles.list, className)}
+      {...props}
+    >
+      <section className={cn(styles.droppable)}>{children}</section>
+
       <footer className={cn(styles.hoursCounter)}>
         Остается <strong>17ч.</strong> для сна
       </footer>
