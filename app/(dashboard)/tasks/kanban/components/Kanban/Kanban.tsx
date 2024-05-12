ffcea1013bb47f502a124805d3822587df4981d2
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-05-12 11:31:11 +0300

    🧊 start using constants for task columns

--- app/(dashboard)/tasks/kanban/components/Kanban/Kanban.tsx
+++ app/(dashboard)/tasks/kanban/components/Kanban/Kanban.tsx
@@ -6,7 +6,7 @@ import { type FC } from 'react';
 
 import { useTaskDnd } from '@/app/(dashboard)/tasks/hooks/useTaskDnd.ts';
 import { useTasks } from '@/app/(dashboard)/tasks/hooks/useTasks.ts';
-import { EnumDndDestId } from '@/src/data/EnumDndDestId.ts';
+import { COLUMNS } from '@/src/data/task-columns.data.ts';
 
 import Column from '../Column';
 
@@ -20,41 +20,14 @@ const Kanban: FC<KanbanProps> = () => {
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className={cn(styles.kanbanHolder)}>
-        <Column
-          destId={EnumDndDestId.TODAY}
-          groupName={'Сегодня'}
-          tasks={tasks}
-        />
-
-        <Column
-          destId={EnumDndDestId.TOMORROW}
-          groupName={'Завтра'}
-          tasks={tasks}
-        />
-
-        <Column
-          destId={EnumDndDestId.ON_THIS_WEEK}
-          groupName={'На этой неделе'}
-          tasks={tasks}
-        />
-
-        <Column
-          destId={EnumDndDestId.ON_NEXT_WEEK}
-          groupName={'На след. неделе'}
-          tasks={tasks}
-        />
-
-        <Column
-          destId={EnumDndDestId.LATER}
-          groupName={'Позже'}
-          tasks={tasks}
-        />
-
-        <Column
-          destId={EnumDndDestId.COMPLETED}
-          groupName={'Выполнено'}
-          tasks={tasks}
-        />
+        {COLUMNS.map(({ label, value }, index) => (
+          <Column
+            key={index}
+            destId={value}
+            groupName={label}
+            tasks={tasks}
+          />
+        ))}
       </div>
     </DragDropContext>
   );
