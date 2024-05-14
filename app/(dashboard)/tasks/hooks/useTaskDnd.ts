bc605a1af7e544b5077e91ec100c49c1dc12b279
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-05-14 13:21:36 +0300

    🈂️ add method to TaskService
    This method allows to share logic for assigning date to new or dropped task

--- app/(dashboard)/tasks/hooks/useTaskDnd.ts
+++ app/(dashboard)/tasks/hooks/useTaskDnd.ts
@@ -2,7 +2,7 @@ import { type DropResult } from '@hello-pangea/dnd';
 
 import { useUpdateTask } from '@/app/(dashboard)/tasks/hooks/useUpdateTask.ts';
 import { type ColumnId, EnumDndDestId } from '@/src/data/EnumDndDestId.ts';
-import { FILTERS } from '@/src/data/task-columns.data.ts';
+import { TaskService } from '@/src/services/task.service.ts';
 
 export const useTaskDnd = () => {
   const { updateTask } = useUpdateTask();
@@ -35,54 +35,7 @@ export const useTaskDnd = () => {
     }
 
     /** New date for dragged item. */
-    let newCreatedAt;
-
-    /** Prevent potential filter collisions. */
-    switch (destColumnId) {
-      case 'on-this-week': {
-        const tomorrow = FILTERS.tomorrow.startOf('day');
-        const endOfWeek = FILTERS[destColumnId].startOf('day');
-        const applicant = tomorrow.add(1, 'day').startOf('day');
-
-        /** Collision detected */
-        if (applicant.isSameOrBefore(endOfWeek)) {
-          applicant.add(1, 'day');
-        }
-
-        newCreatedAt = applicant.startOf('day').toDate();
-        break;
-      }
-
-      // Next week must point to first day in
-      // week, however this day may be the next day (tomorrow).
-      case 'on-next-week': {
-        /** This date is taken from filter directly. */
-        const dateApplicant = FILTERS[destColumnId]
-          .subtract(7, 'day')
-          .add(1, 'day')
-          .startOf('day');
-
-        /** This date equals to tomorrow. */
-        const nextDayDate = FILTERS.tomorrow.startOf('day');
-
-        /** Collision detected */
-        if (dateApplicant.isSame(nextDayDate)) {
-          newCreatedAt = dateApplicant.add(1, 'day').toDate();
-          break;
-        }
-
-        /** No collision */
-        newCreatedAt = dateApplicant.toDate();
-        break;
-      }
-
-      // By default, newCreatedAt is assigned
-      // to exact date from filter.
-      default: {
-        newCreatedAt = FILTERS[destColumnId].toDate();
-        break;
-      }
-    }
+    const newCreatedAt: Date = TaskService.issueDate(destColumnId);
 
     updateTask({
       id: result.draggableId,
--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,8 +1,71 @@
 import { axiosWithAuth } from '@/src/api/interceptors.ts';
+import { type EnumDndDestId } from '@/src/data/EnumDndDestId.ts';
+import { FILTERS } from '@/src/data/task-columns.data.ts';
 import type { ITaskResponse, TaskFormStateType } from '@/src/types';
 
 export class TaskService {
   private static BASE_URL = '/user/task';
+
+  /**
+   * Return date for task that is being drag-n-dropped
+   * or created.
+   * @param destColumnId
+   */
+  static issueDate(
+    destColumnId: Exclude<EnumDndDestId, EnumDndDestId.COMPLETED>,
+  ): Date {
+    /** Date applicant. */
+    let newCreatedAt: Date;
+
+    /** Prevent potential filter collisions. */
+    switch (destColumnId) {
+      case 'on-this-week': {
+        const tomorrow = FILTERS.tomorrow.startOf('day');
+        const endOfWeek = FILTERS[destColumnId].startOf('day');
+        const applicant = tomorrow.add(1, 'day').startOf('day');
+
+        /** Collision detected */
+        if (applicant.isSameOrBefore(endOfWeek)) {
+          applicant.add(1, 'day');
+        }
+
+        newCreatedAt = applicant.startOf('day').toDate();
+        break;
+      }
+
+      // Next week must point to first day in
+      // week, however this day may be the next day (tomorrow).
+      case 'on-next-week': {
+        /** This date is taken from filter directly. */
+        const dateApplicant = FILTERS[destColumnId]
+          .subtract(7, 'day')
+          .add(1, 'day')
+          .startOf('day');
+
+        /** This date equals to tomorrow. */
+        const nextDayDate = FILTERS.tomorrow.startOf('day');
+
+        /** Collision detected */
+        if (dateApplicant.isSame(nextDayDate)) {
+          newCreatedAt = dateApplicant.add(1, 'day').toDate();
+          break;
+        }
+
+        /** No collision */
+        newCreatedAt = dateApplicant.toDate();
+        break;
+      }
+
+      // By default, newCreatedAt is assigned
+      // to exact date from filter.
+      default: {
+        newCreatedAt = FILTERS[destColumnId].toDate();
+        break;
+      }
+    }
+
+    return newCreatedAt;
+  }
 
   static async getTasks() {
     const response = await axiosWithAuth.get<ITaskResponse[]>(this.BASE_URL);
