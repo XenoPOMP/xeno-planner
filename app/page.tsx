d4af747e678a3839f3349c28204038ba9ec0948b
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-05-16 13:21:54 +0300

    🏛️ setup column layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import cn from 'classnames';
 import { type Metadata } from 'next';
 
 import LandingHeader from '@/src/components/layout/landing/LandingHeader';
+import UiContainer from '@/src/components/ui/UiContainer/UiContainer.tsx';
 import { generateOpenGraph } from '@/src/utils/seo';
 
 import styles from './main-page.module.scss';
@@ -24,6 +25,16 @@ export default function Home() {
   return (
     <main className={cn(styles.mainPage)}>
       <LandingHeader />
+
+      <UiContainer
+        as={'section'}
+        maxWidth={'1600px'}
+        margin={'0px'}
+        className={cn(styles.body)}
+      >
+        <article className={cn(styles.column)}></article>
+        <article className={cn(styles.column)}></article>
+      </UiContainer>
     </main>
   );
 }
