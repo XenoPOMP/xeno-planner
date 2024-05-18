b9f8f2218e44a905921a858da5e06b9d30866cfc
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-05-18 10:56:44 +0300

    Revert "✅ allow http requests from https"
    This reverts commit 938370243fe209b8611230e2537d4be42360f5ac.

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,11 +46,6 @@ export async function generateMetadata(): Promise<Metadata> {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='ru'>
-      <meta
-        httpEquiv={'Content-Security-Policy'}
-        content='upgrade-insecure-requests'
-      />
-
       <body className={MainFont.className}>
         <Providers>
           <>
