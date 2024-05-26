e99ef524c42a84aef4c710b4e314d833f08e55f4
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-05-26 17:16:16 +0300

    💬 add typograf to mdx

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,7 @@
 import type { MDXComponents } from 'mdx/types';
 
 import HtmlHeading from '@/src/components/ui/Heading';
+import TypografedText from '@/src/components/ui/TypografedText';
 
 /** Automatically creates MDX components for headings. */
 const getHeadingComponents = () => {
@@ -8,7 +9,9 @@ const getHeadingComponents = () => {
 
   for (let i = 1; i <= 5; i++) {
     components[`h${i}`] = ({ children }) => (
-      <HtmlHeading as={`h${i as 1 | 2 | 3 | 4 | 5}`}>{children}</HtmlHeading>
+      <HtmlHeading as={`h${i as 1 | 2 | 3 | 4 | 5}`}>
+        <TypografedText>{children}</TypografedText>
+      </HtmlHeading>
     );
   }
 
