b9baa3bdf4d5d9407c7dbdc80069386b68c6f8eb
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-05-26 17:05:55 +0300

    🗣️ replace headings in mdx to custom

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,23 @@
 import type { MDXComponents } from 'mdx/types';
 
+import HtmlHeading from '@/src/components/ui/Heading';
+
+/** Automatically creates MDX components for headings. */
+const getHeadingComponents = () => {
+  const components: MDXComponents = {};
+
+  for (let i = 1; i <= 5; i++) {
+    components[`h${i}`] = ({ children }) => (
+      <HtmlHeading as={`h${i as 1 | 2 | 3 | 4 | 5}`}>{children}</HtmlHeading>
+    );
+  }
+
+  return components;
+};
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    ...getHeadingComponents(),
     ...components,
   };
 }
