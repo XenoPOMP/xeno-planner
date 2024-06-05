0bc7266c812edd4cdac8361516b85b4ee812075b
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-06-05 17:04:50 +0300

    🔗 update qr code page seo

--- app/(landing)/share/page.tsx
+++ app/(landing)/share/page.tsx
@@ -1,12 +1,30 @@
 import type { WithSearchParams } from '@xenopomp/advanced-types';
 import cn from 'classnames';
+import { type Metadata } from 'next';
 import { QRCodeSVG } from 'qrcode.react';
 import { type FC, useCallback } from 'react';
 
 import { AppConstants } from '@/app/app.constants.ts';
 import LandingLayout from '@/src/components/layout/landing/LandingLayout';
+import { NO_INDEX_PAGE } from '@/src/constants/seo.constants.ts';
+import { generateOpenGraph } from '@/src/utils/seo';
 
 export const revalidate = 0;
+
+export async function generateMetadata(): Promise<Metadata> {
+  const info = {
+    title: 'Поделиться',
+    description: 'Сканируйте QR-код, чтобы быстро открыть приложение.',
+  };
+
+  return {
+    ...info,
+    openGraph: generateOpenGraph({
+      ...info,
+    }),
+    ...NO_INDEX_PAGE,
+  };
+}
 
 const SharePage: FC<WithSearchParams<{}, 'route'>> = ({ searchParams }) => {
   const CANONICAL = process.env.CANONICAL_URL;
