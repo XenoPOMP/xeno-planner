959aed90608d3a3cc566973389b11a81e5d92172
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-06-05 20:09:11 +0300

    💬 update comments in test

--- __tests__/unit/components/color-badge.test.tsx
+++ __tests__/unit/components/color-badge.test.tsx
@@ -3,15 +3,21 @@ import { describe, test } from 'vitest';
 import { expectToRender } from '@/__tests__/assets/utilities';
 import ColorBadge from '@/src/components/ui/ColorBadge';
 
+// Тесты для компонента ColorBadge
 describe('ColorBadge test', () => {
+  // Ожидаем, что он может быть отрендерен без ошибок
   test('It renders', () => {
     expectToRender(<ColorBadge />);
   });
 
+  // Проверяем, что отключение режима hex (когда цвет
+  // записывается в формате #FFFFFF) работает, и компонент
+  // пытается подобрать название цвета
   test('No hex', () => {
     expectToRender(<ColorBadge hex={false} />);
   });
 
+  // Проверка того, что цвета будут парсится
   test('Colors are being parsed', () => {
     expectToRender(<ColorBadge>tomato</ColorBadge>);
     expectToRender(<ColorBadge>#FFFFFF</ColorBadge>);
