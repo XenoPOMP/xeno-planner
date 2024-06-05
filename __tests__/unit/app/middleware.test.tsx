0ed828892e485e8ad1f257f64e68369ae3fda169
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-06-05 22:16:01 +0300

    💬 update comments in test

--- __tests__/unit/app/middleware.test.tsx
+++ __tests__/unit/app/middleware.test.tsx
@@ -6,6 +6,7 @@ import { clearMocks, mockRouter } from '@/__tests__/assets/mocks';
 import { middleware } from '@/middleware.ts';
 import { DASHBOARD_PAGES } from '@/src/types/routes.ts';
 
+// Тестируем middleware
 describe('Next.js middleware', () => {
   beforeAll(() => {
     mockRouter();
@@ -15,7 +16,7 @@ describe('Next.js middleware', () => {
     clearMocks();
   });
 
-  /** Generates mock request and response objects. */
+  /** Генерирует фейковые объекты запроса и ответа. */
   const issueConfig = (
     requestConfig: ConstructorParameters<typeof NextRequest> = [
       new URL('http://localhost:3000'),
@@ -26,32 +27,38 @@ describe('Next.js middleware', () => {
     return [req, res];
   };
 
+  // Базовая проверка - функция middleware не должнап рокидывать исключения
   test('It not throwing', async (): Promise<void> => {
     assertNotThrowing(async () => await middleware(...issueConfig()));
   });
 
+  // Проверяем, что middleware видит refreshToken
   test('Checks for refreshToken', () => {
     const [req, res] = issueConfig([new URL('http://localhost:3000/auth')]);
+    // Вручную ставим куку refreshToken
     req.cookies.set('refreshToken', 'someToken');
 
-    // Asserts for correct middleware work
+    // middleware все еще не должен не прокидывать исключения
     assertNotThrowing(async () => await middleware(req, res));
   });
 
+  // Покрываем сценарий, когда текущая страница - страница авторизации
   test('Checks if current page is auth', () => {
     const [req, res] = issueConfig([new URL('http://localhost:3000/auth')]);
 
-    // Asserts for correct middleware work
+    // middleware все еще не должен не прокидывать исключения
     assertNotThrowing(async () => await middleware(req, res));
   });
 
+  // Покрываем сценарий, когда пользователь авторизован и пытается зайти
+  // на страницу Личный кабинет
   test('If logged, pass him to page', () => {
     const [req, res] = issueConfig([
       new URL(`http://localhost:3000${DASHBOARD_PAGES.HOME}`),
     ]);
     req.cookies.set('refreshToken', 'someToken');
 
-    // Asserts for correct middleware work
+    // middleware все еще не должен не прокидывать исключения
     assertNotThrowing(async () => await middleware(req, res));
   });
 });
