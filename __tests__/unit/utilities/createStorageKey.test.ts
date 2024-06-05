b4400462fed7287e002b908b072aa2b26d807549
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-06-05 20:37:34 +0300

    💬 update comments in test

--- __tests__/unit/utilities/createStorageKey.test.ts
+++ __tests__/unit/utilities/createStorageKey.test.ts
@@ -3,15 +3,21 @@ import { describe, expect, test } from 'vitest';
 import { AppConstants } from '@/app/app.constants';
 import { createStorageKey } from '@/src/utils/misc';
 
+// Функция, которая создает название для записей в куках,
+// localStorage и т.п. по единому правилу
 describe('createStorageKey func', () => {
+  // Название приложения - константа
   const { appName } = AppConstants;
 
+  // Эта функция нужна для того, чтобы не повторять один
+  // и тот же код в каждом тесте (принцип DRY).
   const generateAndExpect = (expected: string, ...keys: string[]) => {
     const generated = createStorageKey(...keys);
 
     expect(generated).to.equal(expected);
   };
 
+  // Пустые строки отсекаются
   test('Func filters empty strings', () => {
     generateAndExpect(
       `[${appName}]:storage:persist`,
@@ -22,10 +28,12 @@ describe('createStorageKey func', () => {
     );
   });
 
+  // Строки обрезаются (убираются лишние пробелы в начале и конце)
   test('Strings are trimmed', () => {
     generateAndExpect(`[${appName}]:storage:persist`, '  storage  ', 'persist');
   });
 
+  // Пробелы заменяются на нижнее подчеркивание
   test('Any whitespace is replaced with underscore', () => {
     generateAndExpect(
       `[${appName}]:storage_and_persist`,
@@ -33,10 +41,13 @@ describe('createStorageKey func', () => {
     );
   });
 
+  // Весь текст приводится к нижнему регистру
   test('Lowercase', () => {
     generateAndExpect(`[${appName}]:hello_world`, 'Hello World');
   });
 
+  // Запрещенные символы (все те символы, что не являются буквами)
+  // убираются
   test('Forbidden chars are sanitized', () => {
     generateAndExpect(
       `[${appName}]:ya_russkiy_i_mne_povezlo_idk`,
