6b0dc787990473a53bb43b49355706832a4aee8a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-06-05 20:22:11 +0300

    💬 update comments in test

--- __tests__/unit/components/logout.test.tsx
+++ __tests__/unit/components/logout.test.tsx
@@ -6,23 +6,34 @@ import { expectToRender } from '@/__tests__/assets/utilities';
 import RQProvider from '@/src/components/providers/RQProvider/RQProvider.tsx';
 import Logout from '@/src/components/settings/Logout';
 
+// Проверяем кнопку «Выйти из аккаунта»
 describe('Logout component', () => {
+  // Перед выполнением тестов необходимо сделать
+  // моки для библиотеки `next/navigation`, потому что
+  // она может быть недоступна в тестовом окружении
   beforeAll(() => {
     mockRouter();
   });
 
+  // Очищаем отрендеренные компоненты после каждого теста, чтобы
+  // они не мешали друг другу и последующим тестам
   afterEach(() => {
     cleanup();
   });
 
+  // Моки очищаем после выполнения ВСЕХ тестов
   afterAll(() => {
     clearMocks();
   });
 
+  // Простая проверка на то, что компонент может быть отрисован
+  // без ошибок
   test('It renders', () => {
     expectToRender(<Logout />, { wrapper: RQProvider });
   });
 
+  // Слушатель событий должен быть вызван при нажатии на
+  // кнопку
   test('Logout button works', async () => {
     render(<Logout />, { wrapper: RQProvider });
     const button = screen.getByText('Выйти из аккаунта');
