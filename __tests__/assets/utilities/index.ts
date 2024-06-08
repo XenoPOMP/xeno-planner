efc2fa73d4831898f984a68a59ff0df5d0d1e982
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-06-08 21:27:08 +0300

    💁‍♂️ write helper func for testing pages

--- __tests__/assets/utilities/index.ts
+++ __tests__/assets/utilities/index.ts
@@ -5,3 +5,4 @@ export * from './expect-to-render';
 export * from './expectToDeepEqual';
 export * from './stubGlobal';
 export * from './injectMocks.ts';
+export * from './testNextPage.ts';
--- __tests__/unit/app/dashboard/page/dashboard-page.test.tsx
+++ __tests__/unit/app/dashboard/page/dashboard-page.test.tsx
@@ -1,30 +1,8 @@
-import { describe, test } from 'vitest';
+import { describe } from 'vitest';
 
-import { mockRouter } from '@/__tests__/assets/mocks';
-import {
-  expectToRender,
-  injectMocks,
-  testObject,
-} from '@/__tests__/assets/utilities';
-import DashboardPage, {
-  generateMetadata,
-} from '@/app/(dashboard)/dashboard/page.tsx';
-import RQProvider from '@/src/components/providers/RQProvider/RQProvider.tsx';
+import { testNextPage } from '@/__tests__/assets/utilities';
+import DashboardPage from '@/app/(dashboard)/dashboard/page.tsx';
 
 describe('Dashboard page in routing', () => {
-  injectMocks({
-    mockingFn: () => {
-      mockRouter();
-    },
-  });
-
-  test('It renders', () => {
-    expectToRender(<DashboardPage />, {
-      wrapper: RQProvider,
-    });
-  });
-
-  test('Dashboard metadata is correct', async () => {
-    testObject(await generateMetadata());
-  });
+  testNextPage(<DashboardPage />);
 });
