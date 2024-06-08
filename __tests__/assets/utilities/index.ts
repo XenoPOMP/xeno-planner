6e19a2f26fbd5ca06c66e09adae17f2dc96a3d66
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-06-08 21:10:48 +0300

    🧪 create func for injecting mocks

--- __tests__/assets/utilities/index.ts
+++ __tests__/assets/utilities/index.ts
@@ -4,3 +4,4 @@ export { clickAll } from './clickAll';
 export * from './expect-to-render';
 export * from './expectToDeepEqual';
 export * from './stubGlobal';
+export * from './injectMocks.ts';
---
+++ __tests__/assets/utilities/injectMocks.ts
@@ -0,0 +1,39 @@
+import { afterAll, beforeAll } from 'vitest';
+
+import { clearMocks } from '@/__tests__/assets/mocks';
+
+interface InjectMocksOptions {
+  /**
+   * Place all your mocks inside this
+   * function.
+   */
+  mockingFn: () => void;
+
+  /** Select when mocks should be injected. */
+  strategy?: 'beforeAll';
+}
+
+/**
+ * Inserts mocks according to selected strategy
+ * (default is `beforeAll`).
+ *
+ * @example
+ * injectMocks({
+ *   mockingFn: () => {
+ *     mockRouter();
+ *   },
+ * });
+ */
+export const injectMocks = ({
+  mockingFn,
+  strategy = 'beforeAll',
+}: InjectMocksOptions) => {
+  if (strategy === 'beforeAll')
+    beforeAll(() => {
+      mockingFn();
+    });
+
+  afterAll(() => {
+    clearMocks();
+  });
+};
