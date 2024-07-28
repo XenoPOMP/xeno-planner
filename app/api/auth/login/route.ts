1e35e7e1bcaa1dab7fdb1dfbed8c74c690497816
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-07-28 22:46:17 +0300

    ✍️ update routes

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,7 @@
+// api/auth/login
+
+export async function POST(req: Request) {
+  // const body = (await req.json()) as IAuthForm;
+
+  return Response.json({});
+}
--- app/api/health-check/route.ts
+++ app/api/health-check/route.ts
@@ -1,3 +1,7 @@
+// api/health-check
+
+export const revalidate = 120;
+
 export async function GET() {
   return Response.json({ status: 'running' });
 }
