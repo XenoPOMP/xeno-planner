fa9d72815d591604fea4ba8a7e51ae936c852872
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-08-02 06:31:01 +0300

    🚮 remove auth routes

--- app/api/auth/form.schema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-// Schema for login request body
-export const loginFormSchema = z.object({
-  email: z.string(),
-  password: z.string(),
-});
--- app/api/auth/login/route.ts
+++
@@ -1,8 +0,0 @@
-import { loginFormSchema } from '../form.schema.ts';
-
-// api/auth/login
-export async function POST(req: Request) {
-  const body = loginFormSchema.parse(await req.json());
-
-  return Response.json({ body });
-}
