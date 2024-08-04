29aa07b5615354f4f30d55c9b38bc09f19a0f56a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-08-04 11:21:46 +0300

    ✍️ init credentials provider

--- auth.ts
+++ auth.ts
@@ -1,9 +1,21 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 
 import { prisma } from '@/prisma/prisma-client.ts';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async credentials => {
+        console.log(credentials);
+        return {};
+      },
+    }),
+  ],
 });
