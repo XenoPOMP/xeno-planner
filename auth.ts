dcb020efbbb58ea26b4bdfa6b06c954d0b12b926
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-08-04 11:02:57 +0300

    🕶️ add prisma adapter to auth.ts

--- auth.ts
+++ auth.ts
@@ -1,5 +1,9 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth';
 
+import { prisma } from '@/prisma/prisma-client.ts';
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [],
 });
