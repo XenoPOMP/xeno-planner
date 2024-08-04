e9f8fe227576bbd74c35d416f7968b76615c7719
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-08-04 10:42:36 +0300

    👺 add previous config to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,20 @@
-export { auth as middleware } from '@/auth';
+import { type MiddlewareConfig, NextResponse } from 'next/server';
+
+import { auth } from '@/auth';
+
+export default auth(_req => {
+  return NextResponse.next();
+});
+
+export const config: MiddlewareConfig = {
+  matcher: [
+    // '/',
+    '/dashboard',
+    '/pomodoro',
+    '/settings',
+    '/tasks/(.*)',
+    '/time-blocking',
+    '/auth/login',
+    '/auth/register',
+  ],
+};
